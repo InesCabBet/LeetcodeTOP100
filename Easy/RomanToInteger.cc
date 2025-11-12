ce81b50ec1b681da8cde6162b4d7f300e4a7b13d
InesCabBet <icb3rg@gmail.com>
2025-11-12 21:18:24 +0200

    backbone going for especifics

--- Easy/RomanToInteger.cc
+++ Easy/RomanToInteger.cc
@@ -5,3 +5,50 @@
  */
 
 #include <iostream>
+#include <string>
+
+int romanToInteger (std::string roman) {
+  int result = 0;
+  for (int i = 0; i < roman.length(); i++){
+    char c = roman[i];
+    switch (c) {
+      case 'I':
+        result += 1;
+        break;
+      case 'V':
+        result += 5;
+        break;
+      case 'X':
+        result += 10;
+        break;
+      case 'L':
+        result += 50;
+        break;
+      case 'C':
+        result += 100;
+        break;
+      case 'D':
+        result += 500;
+        break;
+      case 'M':
+        result += 100;
+        break;
+      default: 
+        return -1;
+    }
+  }
+  return result;
+}
+
+int main() {
+  std::cout << "Introduce roman number: ";
+  std::string roman;
+  std::cin >> roman;
+  int result = romanToInteger(roman);
+  if (result == -1){
+    std::cout << "Input error. Character not recognised.\nCharacters recognised by the program= I V X L C D M" << std::endl;
+    return 1;
+  }
+  std::cout << "Result: " << result << std::endl;
+  return 0;
+}
